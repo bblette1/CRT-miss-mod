e63c7031ab364e4a55fde6743054643423e8db85
Bryan Blette <bletteb18@gmail.com>
2022-08-27 15:02:14 -0400

    Add Monte Carlo SE output

--- make-sim-fig.R
+++ make-sim-fig.R
@@ -26,11 +26,16 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
   
   # Bias for interaction term
   bias20_int <- colMeans(results[, seq(4, 84, by = 4)], na.rm = T) - true_int
+  bias20_int_mcse <-
+    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T) /
+           (nsims - 1))
   
   # Bias for ATE
   est20_ate <- results[, seq(2, 82, by = 4)] +
                exp(0.5) / (1 + exp(0.5)) * results[, seq(4, 84, by = 4)]
   bias20_ate <- colMeans(est20_ate, na.rm = T) - true_ate
+  bias20_ate_mcse <- sqrt(colMeans((est20_ate - true_ate)^2, na.rm = T) /
+                            (nsims - 1))
   
   # Coverage for interaction term
   cov20_int <- rep(NA, 21)
@@ -48,6 +53,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
               results[, (i + 36)*pl]*results[, (i + 21)*pl] > true_int),
            na.rm = T)
   }
+  cov20_int_mcse <- sqrt(cov20_int*(1 - cov20_int) / nsims)
   
   # Coverage for ATE
   cov20_ate <- rep(NA, 21)
@@ -67,14 +73,20 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est20_ate[, i] + results[, (i + 36)*pl]*se > true_ate),
            na.rm = T)
   }
+  cov20_ate_mcse <- sqrt(cov20_ate*(1 - cov20_ate) / nsims)
   
-  # RMSE for interaction term
-  rmse20_int <-
-    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T))
+  # MSE for interaction term
+  mse20_int <-
+    colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T)
+  mse20_int_mcse <-
+    sqrt(colMeans(((results[, seq(4, 84, by = 4)] - true_int)^2 -
+                rep(mse20_int, each = 1000))^2, na.rm = T) / (nsims - 1))
   
-  # RMSE for ATE
-  rmse20_ate <-
-    sqrt(colMeans((est20_ate - true_ate)^2, na.rm = T))
+  # MSE for ATE
+  mse20_ate <- colMeans((est20_ate - true_ate)^2, na.rm = T)
+  mse20_ate_mcse <-
+    sqrt(colMeans(((est20_ate - true_ate)^2 -
+                rep(mse20_ate, each = 1000))^2, na.rm = T) / (nsims - 1))
   
   # Power for interaction term
   power20_int <- rep(NA, 21)
@@ -92,6 +104,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 results[, (i + 36)*pl]*results[, (i + 21)*pl] < 0),
            na.rm = T)
   }
+  power20_int_mcse <- sqrt(power20_int*(1 - power20_int) / nsims)
   
   # Power for ATE
   power20_ate <- rep(NA, 21)
@@ -111,6 +124,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est20_ate[, i] + results[, (i + 36)*pl]*se < 0),
            na.rm = T)
   }
+  power20_ate_mcse <- sqrt(power20_ate*(1 - power20_ate) / nsims)
   
   ##############################################################
   # Now with 50 clusters
@@ -129,11 +143,16 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
   
   # Bias for interaction term
   bias50_int <- colMeans(results[, seq(4, 84, by = 4)], na.rm = T) - true_int
+  bias50_int_mcse <-
+    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T) /
+           (nsims - 1))
   
   # Bias for ATE
   est50_ate <- results[, seq(2, 82, by = 4)] +
                exp(0.5) / (1 + exp(0.5)) * results[, seq(4, 84, by = 4)]
   bias50_ate <- colMeans(est50_ate, na.rm = T) - true_ate
+  bias50_ate_mcse <- sqrt(colMeans((est50_ate - true_ate)^2, na.rm = T) /
+                            (nsims - 1))
   
   # Coverage for interaction term
   cov50_int <- rep(NA, 21)
@@ -151,6 +170,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 results[, (i + 36)*pl]*results[, (i + 21)*pl] > true_int),
            na.rm = T)
   }
+  cov50_int_mcse <- sqrt(cov50_int*(1 - cov50_int) / nsims)
   
   # Coverage for ATE
   cov50_ate <- rep(NA, 21)
@@ -170,14 +190,21 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est50_ate[, i] + results[, (i + 36)*pl]*se > true_ate),
            na.rm = T)
   }
+  cov50_ate_mcse <- sqrt(cov50_ate*(1 - cov50_ate) / nsims)
   
   # RMSE for interaction term
-  rmse50_int <-
-    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T))
+  mse50_int <-
+    colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T)
+  mse50_int_mcse <-
+    sqrt(colMeans(((results[, seq(4, 84, by = 4)] - true_int)^2 -
+                     rep(mse50_int, each = 1000))^2, na.rm = T) / (nsims - 1))
   
   # RMSE for ATE
-  rmse50_ate <-
-    sqrt(colMeans((est50_ate - true_ate)^2, na.rm = T))
+  mse50_ate <-
+    colMeans((est50_ate - true_ate)^2, na.rm = T)
+  mse50_ate_mcse <-
+    sqrt(colMeans(((est50_ate - true_ate)^2 -
+                     rep(mse50_ate, each = 1000))^2, na.rm = T) / (nsims - 1))
   
   # Power for interaction term
   power50_int <- rep(NA, 21)
@@ -195,6 +222,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 results[, (i + 36)*pl]*results[, (i + 21)*pl] < 0),
            na.rm = T)
   }
+  power50_int_mcse <- sqrt(power50_int*(1 - power50_int) / nsims)
   
   # Power for ATE
   power50_ate <- rep(NA, 21)
@@ -214,6 +242,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est50_ate[, i] + results[, (i + 36)*pl]*se < 0),
            na.rm = T)
   }
+  power50_ate_mcse <- sqrt(power50_ate*(1 - power50_ate) / nsims)
   
   ###############################################################
   # Now with 100 clusters
@@ -232,11 +261,16 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
   
   # Bias for interaction term
   bias100_int <- colMeans(results[, seq(4, 84, by = 4)], na.rm = T) - true_int
+  bias100_int_mcse <-
+    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T) /
+           (nsims - 1))
   
   # Bias for ATE
   est100_ate <- results[, seq(2, 82, by = 4)] +
                 exp(0.5) / (1 + exp(0.5)) * results[, seq(4, 84, by = 4)]
   bias100_ate <- colMeans(est100_ate, na.rm = T) - true_ate
+  bias100_ate_mcse <- sqrt(colMeans((est100_ate - true_ate)^2, na.rm = T) /
+                            (nsims - 1))
   
   # Coverage for interaction term
   cov100_int <- rep(NA, 21)
@@ -254,6 +288,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 results[, (i + 36)*pl]*results[, (i + 21)*pl] > true_int),
            na.rm = T)
   }
+  cov100_int_mcse <- sqrt(cov100_int*(1 - cov100_int) / nsims)
   
   # Coverage for ATE
   cov100_ate <- rep(NA, 21)
@@ -273,14 +308,21 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est100_ate[, i] + results[, (i + 36)*pl]*se > true_ate),
            na.rm = T)
   }
+  cov100_ate_mcse <- sqrt(cov100_ate*(1 - cov100_ate) / nsims)
   
-  # RMSE for interaction term
-  rmse100_int <-
-    sqrt(colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T))
+  # MSE for interaction term
+  mse100_int <-
+    colMeans((results[, seq(4, 84, by = 4)] - true_int)^2, na.rm = T)
+  mse100_int_mcse <-
+    sqrt(colMeans(((results[, seq(4, 84, by = 4)] - true_int)^2 -
+                     rep(mse100_int, each = 1000))^2, na.rm = T) / (nsims - 1))
   
-  # RMSE for ATE
-  rmse100_ate <-
-    sqrt(colMeans((est100_ate - true_ate)^2, na.rm = T))
+  # MSE for ATE
+  mse100_ate <-
+    colMeans((est100_ate - true_ate)^2, na.rm = T)
+  mse100_ate_mcse <-
+    sqrt(colMeans(((est100_ate - true_ate)^2 -
+                     rep(mse100_ate, each = 1000))^2, na.rm = T) / (nsims - 1))
   
   # Power for interaction term
   power100_int <- rep(NA, 21)
@@ -298,6 +340,7 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 results[, (i + 36)*pl]*results[, (i + 21)*pl] < 0),
            na.rm = T)
   }
+  power100_int_mcse <- sqrt(power100_int*(1 - power100_int) / nsims)
   
   # Power for ATE
   power100_ate <- rep(NA, 21)
@@ -317,18 +360,19 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                 est100_ate[, i] + results[, (i + 36)*pl]*se < 0),
            na.rm = T)
   }
+  power100_ate_mcse <- sqrt(power100_ate*(1 - power100_ate) / nsims)
   
   # Summary figure
-  fig_dat_int <-
+  fig_dat <-
     data.frame("Output" = c(rep(bias20_int[1], 4), bias20_int[-1],
                             rep(bias50_int[1], 4), bias50_int[-1],
                             rep(bias100_int[1], 4), bias100_int[-1],
                             rep(cov20_int[1], 4), cov20_int[-1],
                             rep(cov50_int[1], 4), cov50_int[-1],
                             rep(cov100_int[1], 4), cov100_int[-1],
-                            rep(rmse20_int[1], 4), rmse20_int[-1],
-                            rep(rmse50_int[1], 4), rmse50_int[-1],
-                            rep(rmse100_int[1], 4), rmse100_int[-1],
+                            rep(mse20_int[1], 4), mse20_int[-1],
+                            rep(mse50_int[1], 4), mse50_int[-1],
+                            rep(mse100_int[1], 4), mse100_int[-1],
                             rep(power20_int[1], 4), power20_int[-1],
                             rep(power50_int[1], 4), power50_int[-1],
                             rep(power100_int[1], 4), power100_int[-1],
@@ -338,14 +382,14 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                             rep(cov20_ate[1], 4), cov20_ate[-1],
                             rep(cov50_ate[1], 4), cov50_ate[-1],
                             rep(cov100_ate[1], 4), cov100_ate[-1],
-                            rep(rmse20_ate[1], 4), rmse20_ate[-1],
-                            rep(rmse50_ate[1], 4), rmse50_ate[-1],
-                            rep(rmse100_ate[1], 4), rmse100_ate[-1],
+                            rep(mse20_ate[1], 4), mse20_ate[-1],
+                            rep(mse50_ate[1], 4), mse50_ate[-1],
+                            rep(mse100_ate[1], 4), mse100_ate[-1],
                             rep(power20_ate[1], 4), power20_ate[-1],
                             rep(power50_ate[1], 4), power50_ate[-1],
                             rep(power100_ate[1], 4), power100_ate[-1]),
                "Estimand" = rep(c("Int", "ATE"), each = 288),
-               "Metric" = rep(rep(c("Bias", "Coverage", "RMSE", "Power"),
+               "Metric" = rep(rep(c("Bias", "Coverage", "MSE", "Power"),
                                   each = 72),
                               2),
                "Hline" = rep(rep(c(0, 0.95, 0, 0), each = 72), 2),
@@ -360,13 +404,31 @@ makeSimFig <- function(type, nsims, ICC_out, ICC_mod) {
                                    4)), 12), 2),
                "Num_Clusters" = rep(rep(rep(c(20, 50, 100), each = 24), 4), 2))
   
-  return(fig_dat_int)
+  mcse_table <- data.frame("Output" = c(bias20_int_mcse, bias50_int_mcse,
+                                        bias100_int_mcse, cov20_int_mcse,
+                                        cov50_int_mcse, cov100_int_mcse,
+                                        mse20_int_mcse, mse50_int_mcse,
+                                        mse100_int_mcse, power20_int_mcse,
+                                        power50_int_mcse, power100_int_mcse,
+                                        bias20_ate_mcse, bias50_ate_mcse,
+                                        bias100_ate_mcse, cov20_ate_mcse,
+                                        cov50_ate_mcse, cov100_ate_mcse,
+                                        mse20_ate_mcse, mse50_ate_mcse,
+                                        mse100_ate_mcse, power20_ate_mcse,
+                                        power50_ate_mcse, power100_ate_mcse),
+                           "Estimand" = rep(c("Int", "Ate"), each = 12*21),
+                           "Metric" = rep(rep(c("Bias", "Coverage", "MSE",
+                                            "Power"), each = 3*21), 2),
+                           "Method_Model" = c(1))
+  
+  return(list(fig_dat, mcse_table))
   
 }
 
 
 # Plot first simulation scenario results
-figdat1 <- makeSimFig("out", 1000, 0.1, 0.1)
+figdat1 <- makeSimFig("out", 1000, 0.1, 0.1)[[1]]
+tabdat1 <- makeSimFig("out", 1000, 0.1, 0.1)[[2]]
 
 ggplot(data = figdat1[figdat1$Estimand == "Int", ],
        aes(x = Num_Clusters, y = Output, color = Model,
