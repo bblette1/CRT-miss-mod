91b21a7d1c7bce41997fc70b74ab60f36394831a
Bryan Blette <bryanblette@bryans-mbp.dhcp.mc.vanderbilt.edu>
2023-10-13 14:03:45 -0500

    Fix figure axis name

--- data-analysis.R
+++ data-analysis.R
@@ -1120,7 +1120,7 @@ ggplot(data = plotdat_full[plotdat_full$estimand == "ATE", ],
   geom_point() + 
   geom_errorbarh(height = .1) +
   scale_y_discrete(limits = rev(plotdat_full$method[1:6])) +
-  labs(x = 'Interaction coefficient', y = 'Method') +
+  labs(x = 'ATE', y = 'Method') +
   geom_vline(xintercept = 0, color = 'black', linetype = 'dashed',
              alpha = .5) +
   theme_light() +
